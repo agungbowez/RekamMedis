d6e598082bef3a51275c6bc2329d47c5ca1a5adc
agungbowez <agunglaksono.it@gmail.com>
2018-08-25 06:48:27 +0700

    add file login

---
+++ auth/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Login Aplikasi Rekam Medis</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="../_assets/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+    <div id="wrapper">
+        <div class="container">
+            <div align="center" style="margin-top : 200px">
+                <form action="" method="post" class="navbar-form">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user" ></i></span>
+                        <input type="text" name="user" class="form-control" placeholder="user name" autofocus required>
+                    </div>
+
+                     <div class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <input type="password" name="pass" class="form-control" placeholder="password" required>
+                    </div>
+
+                    <div class="input-group">
+                        <input type="submit" name="login" class="btn btn-primary" value="login">
+                    </div>
+
+                </form>
+            </div>
+        </div>   
+    </div>
+   
+</body>
+
+</html>
