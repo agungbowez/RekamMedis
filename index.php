42650fd36ee47ab48bc0c43f834df588addc3717
agungbowez <agunglaksono.it@gmail.com>
2018-09-06 15:11:32 +0700

    create select data obat

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   require_once "_config/config.php"; 
 
-  if(isset($_SESSION['user'])){
+  if(isset($_SESSION['user'])) {
   	// echo "hello world. <a href=\"auth/logout.php\">Logout</a>";
     echo "<script> window.location='".base_url('dashboard')."'; </script>";
   }else{ 
